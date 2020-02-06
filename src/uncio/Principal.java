560b8cdcddcd40ede1f75a12967a98a30e50be35
MiguelAngelCifredo <macifredo@gmail.com>
2020-02-06 13:46:53 +0100

    La documentación

--- src/uncio/Principal.java
+++ src/uncio/Principal.java
@@ -1,7 +1,10 @@
 package uncio;
 
 public class Principal {
-
+	/**
+	 * @author Miguel Angel
+	 * @param ninguno
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //
